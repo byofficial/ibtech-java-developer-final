ab6c30530231a495656f5034ce7134e81c2191e6
Burak YILDIZ <burak@yaztem.com>
2022-09-14 22:08:07 +0300

    Add category list servlet class

---
+++ src/main/java/com/ibtech/mall/web/servlet/category/CategoryListServlet.java
@@ -0,0 +1,35 @@
+package com.ibtech.mall.web.servlet.category;
+
+import com.ibtech.mall.core.XmlHelper;
+import com.ibtech.mall.database.entity.Category;
+import com.ibtech.mall.database.manager.CategoryManager;
+import com.ibtech.mall.xml.CategoryXml;
+import org.w3c.dom.Document;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "CategoryListServlet", value = "/api/categories")
+public class CategoryListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            List<Category> categoryList;
+            CategoryManager categoryManager = new CategoryManager();
+            categoryList = categoryManager.findAll();
+            Document document = CategoryXml.format(categoryList);
+
+            response.setContentType("application/xml;charset=UTF-8");
+
+            XmlHelper.dump(document, response.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
