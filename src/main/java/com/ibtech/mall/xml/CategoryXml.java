ebd187a9a39c8acd0259c9f82f95650ce7fcd48a
Burak YILDIZ <burak@yaztem.com>
2022-09-14 22:07:55 +0300

    Add category xml class

---
+++ src/main/java/com/ibtech/mall/xml/CategoryXml.java
@@ -0,0 +1,56 @@
+package com.ibtech.mall.xml;
+
+import com.ibtech.mall.core.XmlHelper;
+import com.ibtech.mall.database.entity.Category;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryXml {
+    public static Document format(Category category) throws ParserConfigurationException {
+        Document document = XmlHelper.create("category");
+        Element element = document.getDocumentElement();
+        XmlHelper.addSingleElementText(document, element, "categoryId", category.getCategoryId());
+        XmlHelper.addSingleElementText(document, element, "categoryName", category.getCategoryName());
+        return document;
+    }
+
+    public static Document format(List<Category> categories) throws ParserConfigurationException {
+        Document document = XmlHelper.create("categories");
+        Element elementList = document.getDocumentElement();
+
+        for (Category category : categories) {
+            Element element = document.createElement("category");
+            XmlHelper.addSingleElementText(document, element, "categoryId", category.getCategoryId());
+            XmlHelper.addSingleElementText(document, element, "categoryName", category.getCategoryName());
+            elementList.appendChild(element);
+        }
+
+        return document;
+
+    }
+
+    public static Category parse(Document document) {
+        Element element = document.getDocumentElement();
+        long categoryId = XmlHelper.getSingleElementText(element, "categoryId", 0);
+        String categoryName = XmlHelper.getSingleElementText(element, "categoryName", "");
+        return new Category(categoryId, categoryName);
+    }
+
+    public static List<Category> parseList(Document document) {
+        List<Category> newCategoryList = new ArrayList<>();
+        Element elementList = document.getDocumentElement();
+        NodeList categories = elementList.getElementsByTagName("category");
+        for (int i = 0; i < categories.getLength(); i++) {
+            Element element = (Element) categories.item(i);
+            long categoryId = XmlHelper.getSingleElementText(element, "categoryId", 0);
+            String categoryName = XmlHelper.getSingleElementText(element, "categoryName", "");
+            newCategoryList.add(new Category(categoryId, categoryName));
+        }
+        return newCategoryList;
+    }
+}
