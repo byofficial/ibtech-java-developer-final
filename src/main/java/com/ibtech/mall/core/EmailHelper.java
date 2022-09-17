a10c76e963e98124949e3f5cacc51e3b8a7e8693
Burak YILDIZ <burak@yaztem.com>
2022-09-17 19:07:13 +0300

    Add email template

--- src/main/java/com/ibtech/mall/core/EmailHelper.java
+++ src/main/java/com/ibtech/mall/core/EmailHelper.java
@@ -10,6 +10,10 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 public class EmailHelper {
@@ -20,7 +24,7 @@ public class EmailHelper {
     private static final String MAIL_PORT = System.getenv("MAIL_PORT");
     private static Logger logger = LoggerFactory.getLogger(EmailHelper.class);
 
-    public static void sendEmail(String toMailAddress, String mailSubject, String mailBody) {
+    public static void sendEmail(String toMailAddress, String mailSubject, String mailTemplate) {
 
         Properties prop = new Properties();
         prop.put("mail.smtp.host", MAIL_HOST);
@@ -34,9 +38,18 @@ public class EmailHelper {
                         return new PasswordAuthentication(MAIL_USER, MAIL_PASS);
                     }
                 });
-
+        StringBuilder contentBuilder = new StringBuilder();
         try {
+            File pfad = new File("");
+            BufferedReader in = new BufferedReader(new FileReader(pfad.getAbsolutePath() + "\\src\\main\\java\\com\\ibtech\\mall\\core\\template\\" + mailTemplate));
+            String str;
+            while ((str = in.readLine()) != null) {
+                contentBuilder.append(str);
+            }
+            in.close();
 
+
+            String content = contentBuilder.toString();
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(MAIL_FROM));
             message.setRecipients(
@@ -44,13 +57,15 @@ public class EmailHelper {
                     InternetAddress.parse(toMailAddress)
             );
             message.setSubject(mailSubject);
-            message.setText(mailBody);
+            message.setText(content);
 
             Transport.send(message);
 
             logger.info("Email send successfully");
 
         } catch (MessagingException e) {
+            logger.error(e.getMessage());
+        } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
