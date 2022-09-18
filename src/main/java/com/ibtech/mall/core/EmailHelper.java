542c8a37328f3c40010eb8f967f8896d9fdbea83
Burak YILDIZ <burak@yaztem.com>
2022-09-18 03:15:42 +0300

    Refactor email url address

--- src/main/java/com/ibtech/mall/core/EmailHelper.java
+++ src/main/java/com/ibtech/mall/core/EmailHelper.java
@@ -40,7 +40,7 @@ public class EmailHelper {
                 });
         StringBuilder contentBuilder = new StringBuilder();
         try {
-            BufferedReader in = new BufferedReader(new FileReader(System.getProperty("user.dir")+ "\\src\\main\\java\\com\\ibtech\\mall\\core\\template\\" + mailTemplate));
+            BufferedReader in = new BufferedReader(new FileReader(System.getProperty("user.dir")+ "/src/main/java/com/ibtech/mall/core/template/" + mailTemplate));
             String str;
             while ((str = in.readLine()) != null) {
                 contentBuilder.append(str);
